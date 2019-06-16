c90fd3a843367da42e029c7ee1c90401fef992d5
krishna kumar <mekrishnakumarkk@gmail.com>
2019-06-16 13:41:38 +0530

    assects folder commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+      	    "db_host" => "localhost",
+	    "db_username" => "krishnakumark",
+            "db_password"  => "lakshmi1@",
+            "base_url" => "http://localhost/feedbacker/",
+            "site_name" => "Feedbacker",
+          );
+
